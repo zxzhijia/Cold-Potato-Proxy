2a83eb140b1f1bf44dc8fb378737bc53c36adfb4
Jonno Schmidt <jonno@schmidtfam.us>
2015-04-12 00:57:29 -0400

    Added constants file for constant values

---
+++ src/Constants.h
@@ -0,0 +1,37 @@
+/*
+ * Constants.h
+ *
+ *  Created on: Apr 11, 2015
+ *      Author: jonno
+ */
+
+#ifndef SRC_CONSTANTS_H_
+#define SRC_CONSTANTS_H_
+
+namespace Constants {
+	namespace SOCKS {
+		namespace Version {
+			const int V5 = 0x05;
+		};
+		namespace Authentication {
+			const int NoAuth = 0x00;
+			const int Invalid = 0xFF;
+		};
+		namespace Command {
+			const int TCPConnection = 0x01;
+		};
+	};
+	namespace IP {
+		namespace Type {
+			const int IPV4 = 0x01;
+			const int Domain = 0x03;
+			const int IPV6 = 0x04;
+		};
+		namespace Length {
+			const int IPV4 = 4;
+			const int IPV6 = 16;
+		};
+	}
+};
+
+#endif /* SRC_CONSTANTS_H_ */
