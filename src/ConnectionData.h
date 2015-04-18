1528286fe543c65ef07279ce99176e1ad0760b50
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-04-17 21:14:54 -0400

    Moved code efrom ProxyConn to ConnectionData

---
+++ src/ConnectionData.h
@@ -0,0 +1,33 @@
+//
+// Created by jonno on 4/17/15.
+//
+
+#ifndef COLD_POTATO_PROXY_CONNECTIONDATA_H
+#define COLD_POTATO_PROXY_CONNECTIONDATA_H
+
+#include <string>
+#include <netinet/in.h>
+
+enum AddressType {
+    IPV4_ADDRESS,
+    IPV6_ADDRESS,
+    DOMAIN_ADDRESS,
+    INVALID
+};
+
+struct ConnectionData
+{
+    int socket; // The socket.
+    sockaddr_in client; // The address of the client.
+};
+
+struct RequestDetails
+{
+    AddressType addressType;
+    int requestType;
+    std::string address;
+    int port;
+};
+
+
+#endif //COLD_POTATO_PROXY_CONNECTIONDATA_H
